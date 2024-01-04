5c1397a48b633d790aa09a642f76dffc8b9275f1
cxoijve <142479040+cxoijve@users.noreply.github.com>
2024-01-04 17:03:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.49 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+string solution(vector<string>participant, vector<string>completion) {
+	string answer = "";
+
+	unordered_map<string, int> map;
+	for (auto player : participant) {
+		if (map.end() == map.find(player))
+			map.insert(make_pair(player, 1));
+		else
+			map[player]++;
+	}
+	for (auto player : completion)
+		map[player]--;
+
+	for (auto player : participant)
+	{
+		if (map[player] > 0) {
+			answer = player;
+			break;
+		}
+	}
+	return answer;
+}
+int main() {
+	vector<string> part = { "leo","kiki","eden" };
+	vector<string> comp = { "eden","kiki" };
+	cout << solution(part, comp);
+	return 0;
+}
