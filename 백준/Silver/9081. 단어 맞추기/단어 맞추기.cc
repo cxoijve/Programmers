b416d653140e118a633384827cbaad5dba57b50c
Jiye Choi <142479040+cxoijve@users.noreply.github.com>
2024-01-30 21:14:45 +0900

    [Silver I] Title: 단어 맞추기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9081. 단어 맞추기/단어 맞추기.cc
+++ 백준/Silver/9081. 단어 맞추기/단어 맞추기.cc
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
     int T;
     cin >> T;
-
+    
     while(T--){
         string word;
         cin >> word;
-		
-        // 다음 순열이 없다면 이전 순열로 돌아옴
-        if(!next_permutation(word.begin(),word.end())){
-            prev_permutation(word.begin(),word.end());
+        
+        if(!next_permutation(word.begin(), word.end())){
+            prev_permutation(word.begin(), word.end());
         }
         for(auto it : word) cout << it;
         cout << "\n";
