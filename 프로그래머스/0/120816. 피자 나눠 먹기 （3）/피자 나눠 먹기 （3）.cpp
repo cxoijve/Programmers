a36167fbd4be5b97570b4badc49796e118d9149c
Jiye Choi <142479040+cxoijve@users.noreply.github.com>
2024-02-15 00:06:21 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.cpp
@@ -6,9 +6,7 @@ using namespace std;
 int solution(int slice, int n) {
     int answer = 0;
     
-    //두 당 1조각씩 떨어질 때 까지 반복
     while(!(answer*slice/n>=1))
         answer++;
-    
     return answer;
 }
