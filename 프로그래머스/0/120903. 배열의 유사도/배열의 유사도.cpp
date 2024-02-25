b6c0a7fd634f736f97e4df64d196b24212610177
Jiye Choi <142479040+cxoijve@users.noreply.github.com>
2024-02-25 21:50:13 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(vector<string> s1, vector<string> s2) {
+    int answer = 0;
+    set<string> s3(s1.begin(), s1.end());
+    for(int i = 0; i < s2.size(); i ++)
+    {
+        if(s3.find(s2[i])!=s3.end())
+        {
+            answer++;
+        }
+    }
+
+
+    return answer;
+}
