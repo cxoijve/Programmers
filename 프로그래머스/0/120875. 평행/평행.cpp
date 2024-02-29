134ea2fff24a09fbe17530725180fb61dde69191
Jiye Choi <142479040+cxoijve@users.noreply.github.com>
2024-03-01 03:11:42 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.cpp
+++ 프로그래머스/0/120875. 평행/평행.cpp
@@ -1,17 +1,40 @@
 #include <string>
 #include <vector>
-
+ 
 using namespace std;
-
-double gradient(int x1, int x2, int y1, int y2){  // 기울기 구하기
-    return (double)(x2 - x1) / (double)(y2 - y1);
-}
-
-int solution(vector<vector<int>> dots) {
+ 
+// 평행이 되면 1 리턴
+bool slope(pair<vector<int>, vector<int>> a, pair<vector<int>, vector<int>> b)
+{
+    float x1 = a.first[0] - a.second[0];
+    float y1 = a.first[1] - a.second[1];
     
-    if(gradient(dots[0][0], dots[1][0], dots[0][1], dots[1][1]) == gradient(dots[2][0], dots[3][0], dots[2][1], dots[3][1])) return 1;
-    if(gradient(dots[0][0], dots[2][0], dots[0][1], dots[2][1]) == gradient(dots[1][0], dots[3][0], dots[1][1], dots[3][1])) return 1;
-    if(gradient(dots[0][0], dots[3][0], dots[0][1], dots[3][1]) == gradient(dots[2][0], dots[1][0], dots[2][1], dots[1][1])) return 1;
+    float x2 = b.first[0] - b.second[0];
+    float y2 = b.first[1] - b.second[1];
+    
+    if ( y1 / x1 == y2 / x2)
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+ 
+ 
+int solution(vector<vector<int>> dots) {
+    vector<int> a = dots[0];
+    vector<int> b = dots[1];
+    vector<int> c = dots[2];
+    vector<int> d = dots[3];
+    
+    // [a-b, c-d], [a-c, b-d], [a-d, b-c] 조합에서 하나라도 평행이 나올 경우
+    if (slope(make_pair(a, b), make_pair(c, d))
+        || slope(make_pair(a, c), make_pair(b, d))
+        || slope(make_pair(a, d), make_pair(b, c))
+        )
+        {
+            return 1;
+        }
     
     return 0;
 }
