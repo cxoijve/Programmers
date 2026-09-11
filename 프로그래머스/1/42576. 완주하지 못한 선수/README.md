09f3561e71fd3c369927d50ed44638aa126468ed
Jiye Choi <142479040+cxoijve@users.noreply.github.com>
2026-09-11 09:58:56 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 39.46 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 36.9 MB, 시간: 38.27 ms
+메모리: 37.1 MB, 시간: 39.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 10일 23:21:47
+2026년 09월 11일 09:58:52
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,16 @@
 def solution(participant, completion):
+    # 1. Hash dict 만들기
     hashDict = {}
     sumHash = 0
     
-    # Participant의 sum(hash) 구하기
+    # 2. Participant 의 sum hash 구하기
     for part in participant:
         hashDict[hash(part)] = part
         sumHash += hash(part)
     
-    # completion의 sum(hash) 빼기
+    # 3. Completion 의 sum hash 빼기
     for comp in completion:
         sumHash -= hash(comp)
-
+    
+    # 4. 남은 값 리턴
     return hashDict[sumHash]
