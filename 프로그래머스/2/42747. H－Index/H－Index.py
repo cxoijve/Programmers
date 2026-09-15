074fc2f771c81d07cfc3389942dbbd5ec0117d47
Jiye Choi <142479040+cxoijve@users.noreply.github.com>
2026-09-15 13:47:41 +0900

    [level 2] Title: H-Index, Time: 0.15 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,8 +2,8 @@ def solution(citations):
     answer = 0
     citations.sort(reverse=True)
     
-    for i in range(len(citations)):              # H_index가 존재하고 H_index를 넘는 논문이 몇 개인지 구할 때
+    for i in range(len(citations)):            
         if(citations[i] < i+1):
             return i
 
-    return len(citations)                        # 인용 횟수가 모두 같을때는 전체를 return
+    return len(citations) 
